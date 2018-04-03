ed89a497c6f2d80cde8544cd4cafd459c707e2de
Shashishekhar Hasabnis <shashishekhar1001@gmail.com>
2018-04-03 13:01:43 +0530

    First GET

--- static/js/my_scripts/controllers/list_controller.js
+++ static/js/my_scripts/controllers/list_controller.js
@@ -1,7 +1,16 @@
 (function(){
     angular
         .module("turtle_facts")
-        .controller("list_controller", function(){
-            this.dummy_data = "Hello World!";
+        .controller("list_controller", function($http, $scope, $q){
+            $http({
+                method : "GET",
+                url : "/api/turtles/"
+            }).then(function mySuccess(response) {
+                $scope.data = response.data;
+                console.log($scope.data);
+            }, function myError(response) {
+                $scope.error = response.statusText;         
+                console.log("Error");       
+            });
         });
 })();
