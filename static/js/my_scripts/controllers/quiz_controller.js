748a5af0b3a10fd45b6b75d07bb080a90f00a634
Shashishekhar Hasabnis <shashishekhar1001@gmail.com>
2018-04-06 03:37:51 +0530

    Hide All Questions on Giving all answers

--- static/js/my_scripts/controllers/quiz_controller.js
+++ static/js/my_scripts/controllers/quiz_controller.js
@@ -1,7 +1,11 @@
 app.controller("quiz_controller", function($http, $scope, $q, manage_state, data_service){
     this.state = manage_state;
     this.quiz = data_service;
-    this.total_answered_questions = 0;//Initially
+    //Initially
+    this.error = false;
+    this.finalize = false;
+    this.total_answered_questions = 0;
+    this.unanswerd_questions = "ALL"
     
     this.quiz.get_quiz().then(function(response){
         $scope.quiz_data = response.data;
@@ -12,26 +16,45 @@ app.controller("quiz_controller", function($http, $scope, $q, manage_state, data
 
     this.active_question = 0;
     
-    this.set_active_question = function(){
-        var braekout = false;
-        var limit = $scope.quiz_data.length - 1;
+    this.set_active_question = function(index){
+        if(index === undefined)
+        {
+            var braekout = false;
+            var limit = $scope.quiz_data.length - 1;
 
-        while(!braekout){
-            this.active_question = this.active_question < limit?++this.active_question:0;
-            
-            if($scope.quiz_data[this.active_question].selected === null){
-                braekout = true;
+            while(!braekout){
+                this.active_question = this.active_question < limit?++this.active_question:0;
+                
+                if(this.active_question===0){
+                    this.error = true;
+                }
+
+                if($scope.quiz_data[this.active_question].selected === null){
+                    braekout = true;
+                }
             }
+        }else{
+            this.active_question = index;
         }
     };
 
     this.next_triggered = function(){
         console.log("Next Triggered.");
         if($scope.quiz_data[this.active_question].selected !== null){
-            this.total_answered_questions++;
+            if(this.total_answered_questions < $scope.quiz_data.length){
+                this.total_answered_questions++;
+            }
             this.unanswerd_questions = $scope.quiz_data.length - this.total_answered_questions;
             if(this.total_answered_questions >= $scope.quiz_data.length){
-                //Finalise the quiz
+                for(var i = 0; i < $scope.quiz_data.length; i++){
+                    if($scope.quiz_data[i].selected === null){
+                        this.set_active_question(i);
+                        return;
+                    }
+                }
+                this.error = false;
+                this.finalize = true;
+                return;
             }
         }
 
